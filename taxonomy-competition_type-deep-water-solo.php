a9f27d36b212263ffc95b9edb92ab9ba5f70040b
accrane <austincrane@Austins-MacBook-Pro-2.local>
2019-12-18 14:49:26 -0500

    fix routesetters

--- taxonomy-competition_type-deep-water-solo.php
+++ taxonomy-competition_type-deep-water-solo.php
@@ -131,6 +131,7 @@ $comingSoon = get_field('coming_soon_Athletes', 'option');
 	<h2>Routesetters</h2>
 </section>
 	<?php
+		$i=0;
 		$wp_query = new WP_Query();
 		$wp_query->query(array(
 		'post_type'=>'athlete',
@@ -143,29 +144,53 @@ $comingSoon = get_field('coming_soon_Athletes', 'option');
 			)
 		)
 	));
-	if ($wp_query->have_posts()) : while ($wp_query->have_posts()) :  $wp_query->the_post(); ?>	
+	if ($wp_query->have_posts()) : while ($wp_query->have_posts()) :  $wp_query->the_post(); $i++;
 
 
-		<article id="<?php echo $hash; ?>" <?php post_class(); ?>>
-				<div class="featured-image">
-				<header class="entry-header">
-					<?php the_title( '<h1 class="">', '</h1>' ); ?>
-				</header><!-- .entry-header -->
-					<?php 
-					if(has_post_thumbnail()) {
-						the_post_thumbnail('tile');
-					} else { ?>
-						<img src="<?php echo $comingSoon['url']; ?>">
-					<?php } ?>
-				</div>
-				
-				<div class="copy">
-					<div class="entry-content">
-						<?php the_content(); ?>
-					</div><!-- .entry-content -->
-					<div class="offset-border"></div>
-				</div>
-			</article><!-- #post-## -->
+		$hash = sanitize_title_with_dashes(get_the_title());
+				$eventComingSoon=get_field('coming_soon');
+				// $eventComingSoon=$eventComingSoon[0];
+				if($i==2) {
+					$class='rightz';
+					$i=0;
+				} else {
+					$class='leftz';
+				}
+
+		?>	
+
+
+		<article id="<?php echo $hash; ?>" class="basic <?php echo $class; ?>">
+					<div class="featured-image-mobile js-tileinfo">
+						<header class="">
+							<?php the_title( '<h2 class="mobile-title">', '</h2>' ); ?>
+						</header><!-- .entry-header -->
+						<?php 
+						if(has_post_thumbnail()) {
+							the_post_thumbnail('tile');
+						} else { ?>
+							<img src="<?php echo $comingSoonImage['url']; ?>">
+						<?php } ?>
+					</div>
+					<div class="copy">
+						<header class="entry-header">
+							<?php the_title( '<h1 class="">', '</h1>' ); ?>
+						</header><!-- .entry-header -->
+						<div class="art-close js-closecopy"><i class="fal fa-times  fa-2x"></i></div>
+						<div class="entry-content">
+							<?php the_content(); ?>
+						</div><!-- .entry-content -->
+						<!-- <div class="offset-border"></div> -->
+						<div class="featured-image">
+							<?php 
+							if(has_post_thumbnail()) {
+								the_post_thumbnail('tile');
+							} else { ?>
+								<img src="<?php echo $comingSoonImage['url']; ?>">
+							<?php } ?>
+						</div>
+					</div>
+				</article><!-- #post-## -->
 
 
 	<?php endwhile; endif; endif;?>
